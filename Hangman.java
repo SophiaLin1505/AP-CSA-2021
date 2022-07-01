748361438e7075ad8cb53875d8e579afffb1649a
Sophia Lin <95647522+SophiaLin1505@users.noreply.github.com>
2022-07-01 09:46:21 -0500

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -39,6 +39,63 @@ public static int randomGuess(){
   int r = (int)(Math.random()*15 + 1);
   return r;
 }
+  //the Hangman class is below, this is called in the main 
+  
+  
+  
+ import java.util.*;
+
+
+public class Hangman
+{
+  // the secret word
+  private String word;
+  
+  // a string that shows which letters in word have and have not been guessed yet
+  private String hint;
+  
+  // how many guesses the player has left
+  private int numGuessesLeft;
+  int secondsLeft = 0;
+ 
+  
+  public Hangman(String secret, int numGuesses)
+  {
+    this.word = secret;
+    this.numGuessesLeft = numGuesses;
+    
+    initHint();
+  }
+  
+  /** Initialize the hint instance variable so that it contains
+   *  a ? for each letter in word. If word is "hello" then hint should be "?????"
+   */
+  public void initHint()
+  {
+    hint = "";
+    for(int i = 0; i<this.word.length(); i++){
+      hint = "?"+ hint;
+    }
+
+  }
+  
+  /** Checks if word contains letter. If it does, return true; 
+   *  otherwise, decrement numGuessesLeft by 1 and return false.
+   *  Precondition: letter contains exactly 1 character
+   */
+  public boolean checkGuess(String letter)
+  { 
+    
+    if(word.indexOf(letter) > -1){
+      return true;
+    } else{
+      this.numGuessesLeft --;
+      return false;
+    }
+      
+    
+    
+  }
 
 
 
