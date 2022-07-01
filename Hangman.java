4342f40d05d820e1c8defe6f447021c447f19859
Sophia Lin <95647522+SophiaLin1505@users.noreply.github.com>
2022-07-01 09:44:32 -0500

    Create Hangman.java

---
+++ Hangman.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+// My plus one is importing the en.txt file so everyone who plays gets a random word, and then to make this game a game of pure chance I also made a random number of guesses each time the user plays.  
+class Main {
+  public static void main(String[] args) {
+    String words = randomWord(importFile("ene.txt"));
+    int r = randomGuess();
+    Hangman game = new Hangman(words, r);
+    game.play();
+    
+  }
+
+public static ArrayList<String> importFile(String fileName){
+    ArrayList<String> words = new ArrayList<>();
+    try {
+      File myObj = new File(fileName);
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        words.add(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    return words;
+  }
+
+  public static String randomWord(ArrayList<String> commonW){
+    String randomWord = "";
+    int r = (int)(Math.random()*commonW.size());
+    randomWord = commonW.get(r);
+    return randomWord;
+}
+public static int randomGuess(){
+  int r = (int)(Math.random()*15 + 1);
+  return r;
+}
+
+
+
